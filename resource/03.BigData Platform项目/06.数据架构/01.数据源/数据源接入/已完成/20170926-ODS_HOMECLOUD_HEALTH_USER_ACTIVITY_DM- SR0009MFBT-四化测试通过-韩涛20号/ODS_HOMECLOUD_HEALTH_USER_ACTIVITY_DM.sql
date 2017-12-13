10d0d3c0f009c7cd651bc85737c2559b9b2cd00d
Choi <cszcszohye@qq.com>
2017-12-13 13:24:19 +0800

    Update

--- resource/03.BigData Platform项目/06.数据架构/01.数据源/数据源接入/已完成/20170926-ODS_HOMECLOUD_HEALTH_USER_ACTIVITY_DM- SR0009MFBT-四化测试通过-韩涛20号/ODS_HOMECLOUD_HEALTH_USER_ACTIVITY_DM.sql
+++
@@ -1,4 +0,0 @@
-ODS_HOMECLOUD_HEALTH_USER_ACTIVITY_DM
-新增字段：
-alter table ODS_HOMECLOUD_HEALTH_USER_ACTIVITY_DM add columns (completeTimes string comment '活动完成次数');  
-alter table ODS_HOMECLOUD_HEALTH_USER_ACTIVITY_DM add columns (campaignTimes string comment '活动剩余抽奖次数');  
